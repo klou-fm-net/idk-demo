1292d5dcd320dc89111571ef8394990b48dd5942
Kevin Lou <klou2fastmail.net>
2025-05-29 10:33:40 -0400

    kubectl_layer = KubectlV27Layer(self, "KubectlLayer")

--- resources/eks_cluster.py
+++ resources/eks_cluster.py
@@ -6,10 +6,11 @@ from constructs import Construct
 from aws_cdk.lambda_layer_kubectl_v27 import KubectlV27Layer
 
 def create_eks_ckuser_and_service(scope, vpc): 
+        kubectl_layer = KubectlV27Layer(scope, "KubectlLayer")
         cluster = eks.Cluster(
             scope, "MyEksCluster",
-            version=eks.KubernetesVersion.V1_27,
             vpc=vpc,
-            kubectl_layer=KubectlV27Layer(scope, "KubectlLayer"),
-            default_capacity=1
+            version=eks.KubernetesVersion.V1_27,
+            default_capacity=1,  # uses t3.small by default
+            kubectl_layer=kubectl_layer  # required
         )
