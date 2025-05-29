a00f94c5db056c9285a46b41304be10c830f04b5
Kevin Lou <klou2fastmail.net>
2025-05-28 22:21:30 -0400

    KubectlV32Layer - 2

--- resources/eks_cluster.py
+++ resources/eks_cluster.py
@@ -1,8 +1,8 @@
 from aws_cdk import (
    aws_eks as eks,
    aws_ec2 as ec2,
-   lambda_layer_kubectl_v32 as KubectlV32Layer
 )
+from aws_cdk.lambda_layer_kubectl_v32 import KubectlV32Layer
 
 def create_eks_ckuser_and_service(scope, vpc): 
     eks.Cluster(scope, "DemoAppEKS",
