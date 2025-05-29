ed0a6c48295af7f5a2f474dc90fe040ec53d8d42
Kevin Lou <klou2fastmail.net>
2025-05-28 22:51:16 -0400

    none

--- resources/eks_cluster.py
+++ resources/eks_cluster.py
@@ -10,6 +10,7 @@ def create_eks_ckuser_and_service(scope, vpc):
             version=eks.KubernetesVersion.V1_27,
             vpc=vpc,
             default_capacity=1,
-            default_capacity_instance=ec2.InstanceType("t2.micro")
+            default_capacity_instance=ec2.InstanceType("t2.micro"),
+            kubectl_layer = None
             # No kubectl_layer here
         )
