af3a21fd887c80d46547941553dc9321941a4c46
Kevin Lou <klou2fastmail.net>
2025-05-29 10:26:44 -0400

    lambda-layer-kubectl-v27

--- resources/eks_cluster.py
+++ resources/eks_cluster.py
@@ -2,15 +2,14 @@ from aws_cdk import (
    aws_eks as eks,
    aws_ec2 as ec2,
 )
-# from aws_cdk.lambda_layer_kubectl import KubectlLayer
+from constructs import Construct
+from aws_cdk.lambda_layer_kubectl_v27 import KubectlV27Layer
 
 def create_eks_ckuser_and_service(scope, vpc): 
         cluster = eks.Cluster(
             scope, "MyEksCluster",
             version=eks.KubernetesVersion.V1_27,
             vpc=vpc,
-            default_capacity=1,
-            default_capacity_instance=ec2.InstanceType("t2.micro"),
-            kubectl_layer = None
-            # No kubectl_layer here
+            kubectl_layer=KubectlV27Layer(scope, "KubectlLayer"),
+            default_capacity=1
         )
--- stacks/api_lambda_iam_ssm_dynamodn_eks_stack.py
+++ stacks/api_lambda_iam_ssm_dynamodn_eks_stack.py
@@ -1,6 +1,5 @@
 from aws_cdk import (
     Stack,
-    Duration,
     aws_iam as iam,
     aws_lambda as lambdafunc,
     aws_ssm as ssm,
@@ -10,9 +9,9 @@ from aws_cdk import (
 
 
 from constructs import Construct
-from resources.lambda_function import create_lambda_function
-from resources.dynamodb_db import create_dynamodb_table
-from resources.eks_cluster import create_eks_ckuser_and_service
+# from resources.lambda_function import create_lambda_function
+# from resources.dynamodb_db import create_dynamodb_table
+# from resources.eks_cluster import create_eks_ckuser_and_service
 
 class KevinLDemoStack(Stack): 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
@@ -20,8 +19,16 @@ class KevinLDemoStack(Stack):
        # dynamodb_table = create_dynamodb_table(self)
        # lambda_function = create_lambda_function(self, dynamodb_table)
         # iam_role = create_iam_role(self)
-        vpc = ec2.CfnVPC(self, "eks_vpc")
-        eks_cluster = create_eks_ckuser_and_service (self, vpc)
+        # vpc = ec2.CfnVPC(self, "eks_vpc")
+        # eks_cluster = create_eks_ckuser_and_service (self, vpc)
+        vpc = ec2.Vpc(self, "EksVpc", max_azs=2)
+
+        cluster = eks.Cluster(
+            self, "EksCluster",
+            vpc=vpc,
+            version=eks.KubernetesVersion.V1_27,
+            default_capacity=1  # This uses t3.small by default
+        )
 
 
 
